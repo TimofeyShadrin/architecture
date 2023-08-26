75328b0792b2e5aa3df4076aac7fa56d85e6a49f
TimofeiShadrin <tshadrin@yandex.ru>
2023-08-26 22:08:30 +0300

    lesson 1 added

---
+++ lesson_01/src/main/java/ru/tshadrin/architecture/lesson_one/App.java
@@ -0,0 +1,13 @@
+package ru.tshadrin.architecture.lesson_one;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
